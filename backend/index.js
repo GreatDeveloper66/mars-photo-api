55402b45d702c231868f551168f083a2e3426c8a
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-04 15:52:56 -0500

    Add require line for dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
+//mongo connection
+mongoose.Promise = global.Promise
+mongoose.connect('')
+require('dotenv').config()
 const app = express()
 const PORT = 3000
-
 app.get('/', (req,res) => {
     res.send(`Our mars application is running on ${PORT}`)
 })
