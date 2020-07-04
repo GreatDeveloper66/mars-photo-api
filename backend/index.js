a0febed31b9be4137bd1d1963129657b9a79b80e
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-04 16:06:20 -0500

    Add mongoose connection to server.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,19 @@ mongoose.connect('')
 require('dotenv').config()
 const app = express()
 const PORT = 3000
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.API_KEY 
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 app.get('/', (req,res) => {
     res.send(`Our mars application is running on ${PORT}`)
 })
