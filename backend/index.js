26ff62f4ba65c78f4ad50a192e583237de3e3656
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-04 13:59:35 -0500

    Add import express command to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose'
 const app = express()
 const PORT = 3000
 
 app.get('/', (req,res) => {
-    res.send(`Our mars application is running ${PORT}`)
+    res.send(`Our mars application is running on ${PORT}`)
 })
 
 app.listen(PORT, () => {
