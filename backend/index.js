9ba9752e23bb9fd2d955770cf6a49020acebc51a
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-06 18:22:48 -0500

    Add Mars API Key and rover cameras to  .env

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 const app = express()
-const PORT = 3000
+const PORT = 5000
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 dotenv.config()
