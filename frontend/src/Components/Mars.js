b4499daf7464d1767732a5541f66d9d23379f7d0
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-09 21:36:18 -0500

    Add form control to bottom row in container of Mars component

--- frontend/src/Components/Mars.js
+++ frontend/src/Components/Mars.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
+import FormControl from '@material-ui/core/FormControl';
 
 class Mars extends Component {
     constructor(props){
@@ -7,10 +8,25 @@ class Mars extends Component {
     }
     render(){
         return(
-            <Grid container justify="center">
-                <Grid item xs={12} sm={6} justify="center">
-                    <h1>MARS PHOTO API</h1>
+            
+            <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
+                <Grid item xs={3} justify="center" alignItems="center">
+                    <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
+                        <h1>MARS PHOTO API</h1>
+                    </Grid>
+                    <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
+                        <h3>CURIOSITY</h3>
+                    </Grid>
+                    <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
+                        <form>
+                            <FormControl>
+
+                            </FormControl>
+                        </form>
+                    </Grid>
                 </Grid>
+                
+
             </Grid>
         )
     }
