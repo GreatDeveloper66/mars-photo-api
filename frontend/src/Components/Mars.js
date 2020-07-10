d1fa59a219dd098a28720565857973d751c8da67
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-09 20:45:50 -0500

    Add Mars, Register and SignIn components

---
+++ frontend/src/Components/Mars.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Mars extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>mars</div>
+        )
+    }
+}
+
+export default Mars
---
+++ frontend/src/Components/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>Sign In</div>
+        )
+    }
+}
+
+export default SignIn
