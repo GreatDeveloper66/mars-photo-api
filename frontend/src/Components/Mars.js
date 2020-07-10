487ae5be75da4092af8dc40e2a65ccddc0f6c73a
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-09 21:49:49 -0500

    Add Drop Down to Mars API form

--- frontend/src/Components/Mars.js
+++ frontend/src/Components/Mars.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
 import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
 
 class Mars extends Component {
     constructor(props){
         super(props)
+        this.state = {
+
+        }
+    }
+
+    handleChange = () => {
+
     }
     render(){
         return(
@@ -20,7 +29,18 @@ class Mars extends Component {
                     <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
                         <form>
                             <FormControl>
-
+                            <InputLabel htmlFor="age-native-simple">Age</InputLabel>
+                            <Select native value={this.state.age} onChange={this.handleChange}
+                                inputProps={{
+                                    name: 'age',
+                                    id: 'age-native-simple',
+                                }}
+                            >
+                            <option aria-label="None" value="" />
+                            <option value={10}>Ten</option>
+                            <option value={20}>Twenty</option>
+                            <option value={30}>Thirty</option>
+                            </Select>
                             </FormControl>
                         </form>
                     </Grid>
