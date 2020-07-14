63a53e5c202cd82b5e03a27f54a525932fde6df3
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-14 17:16:52 -0500

    Add bootstrap import statement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Mars from './Components/Mars'
 import Register from './Components/Register'
 import SignIn from './Components/SignIn'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 
 function App() {
