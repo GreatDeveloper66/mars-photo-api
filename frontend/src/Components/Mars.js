810da77066577302185e5eef30d8d3205771a6fc
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-14 18:13:34 -0500

    refactor code for form

--- frontend/src/Components/Mars.js
+++ frontend/src/Components/Mars.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react'
-import { Row, Container, Col } from 'react-bootstrap'
+import { Row, Container, Col, Form } from 'react-bootstrap'
 
 export default function Mars() {
     const [ camera, setCamera ] = useState('FHAZ')
@@ -11,24 +11,41 @@ export default function Mars() {
         console.log('sol', sol)
     }
 
-        return(
-            
-            <Container>
-            <Row>
-              <Col>1 of 2</Col>
-              <Col>2 of 2</Col>
-            </Row>
-            <Row>
-              <Col>1 of 3</Col>
-              <Col>2 of 3</Col>
-              <Col>3 of 3</Col>
-            </Row>
-          </Container>
-                        
-                           
-                          
-                            
-                        
-                  
-        )
+    return(
+        <Container fluid>
+            <Row className="d-flex justify-content-center">
+              <Col xs={12} sm={4} lg={3} className="d-flex justify-content-center">
+              <Form> 
+                <Form.Row> 
+                    <h1>MARS PHOTO API</h1>
+                </Form.Row> 
+                <Form.Row> 
+                    <h3>CURIOSITY</h3> 
+                </Form.Row> 
+                <Form.Row> 
+                    <label htmlFor="sol">Sol</label> 
+                    <input type="number"></input> 
+                </Form.Row> 
+                <Form.Row> 
+                <label htmlFor="camera">Camera</label> 
+                <select id="cameras" name="cameras" value={camera} onChange={handleChange}> 
+                    <option value="FHAZ">FHAZ</option> 
+                    <option value="RHAZ">RHAZ</option> 
+                    <option value="MAST">MAST</option> 
+                    <option value="CHEMCAM">CHEMCAM</option> 
+                    <option value="MAHLI">MAHLI</option> 
+                    <option value="MARDI">MARDI</option> 
+                    <option value="NAVCAM">NAVCAM</option> 
+                    <option value="PANCAM">PANCAM</option> 
+                    <option value="MINITES">MINITES</option> 
+                </select> 
+            </Form.Row> 
+            <Form.Row> 
+                <button type="submit" onSubmit={handleSubmit}>Find Photos</button> 
+            </Form.Row> 
+        </Form> 
+    </Col>
+</Row>
+</Container>   
+)
 }
