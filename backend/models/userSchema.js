1c80fa8cfe90b068a6e136b04f629a7dbbc26a5d
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-16 20:26:18 -0500

    Add user schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -2,5 +2,16 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const UserSchema = new Schema({
-    
+    id: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
 })
