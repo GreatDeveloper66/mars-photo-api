e89cc11de12d13ca87c7786677e698a3e98cac02
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-16 20:22:56 -0500

    Add mars schema

--- backend/models/marsModels.js
+++ backend/models/marsModels.js
@@ -2,5 +2,12 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 export const MarsSchema = new Schema({
-    
+    id: {
+        type: Number,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    }
 })
