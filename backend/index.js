e9edc3ada7a9619069ff559a5bf0ba0116098d2e
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-21 19:13:06 -0500

    comment out mongoose import

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import express from 'express'
-import mongoose from 'mongoose'
+//import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import fetch from 'isomorphic-fetch'
 /*
